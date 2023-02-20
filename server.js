2315a20e695f27b6735e4b0dc7bb9470423f3dc5
Ikbal Maulana | iblmln <90613431+iblmln@users.noreply.github.com>
2023-02-20 15:10:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express")
+const express = require("express") // Constant: Require express
 
-const server = express()
+const server = express() // Constant: The server uses express in operation
 
-server.all("/", (req, res) => {
-  res.send("Bot is running!")
+server.all("/", (req, res) => { // Then the server will be on "/" which means the first page or homepage
+  res.send("Bot is running!") // Informs that the server is running which will appear on the website
 })
 
 function keepAlive() {
   server.listen(3000, () => {
-    console.log("Server is ready.")
+    console.log("Server is ready.") // The console logs that the server has been ready
   })
 }
 
-module.exports = keepAlive
+module.exports = keepAlive // make it a module
